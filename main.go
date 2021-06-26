6bb2c41b4c09c4d426378ba8e5659e8c96feae24
DimasAdityaGH <dimasubuntu21@gmail.com>
2021-06-26 17:44:22 +0700

    menambah code operator matematika

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 10
+	var c = a + b
+	fmt.Println(c)
+
+	var result = 10 * 10
+	fmt.Println(result)
+
+	var i = 10
+	i++
+	fmt.Println(i)
+
+	var j = 10
+	j += 10 //j = j + 10
+	fmt.Println(j)
+}
